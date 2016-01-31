2145f1dc70c3bc5a45ceb9528df105ba49fcd7a0
molnplus <wang.hu.molnplus@gmail.com>
2016-02-01 00:50:34 +0800

    hoover updated freemind

---
+++ Das Projekt e1071.mm
@@ -0,0 +1,30 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1454258455842" ID="ID_930819187" MODIFIED="1454258719320" TEXT="Das Projekt e1071">
+<font NAME="PingFang TC" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1454258714947" ID="ID_63776443" MODIFIED="1454258728601" POSITION="right" TEXT="purpose">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="PingFang TC" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1454258730058" ID="ID_86735608" MODIFIED="1454258742900" TEXT="combine svm with r">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1454258760036" ID="ID_362014449" MODIFIED="1454258764375" TEXT="classification">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="PingFang TC" SIZE="16"/>
+<node COLOR="#990000" CREATED="1454258766271" ID="ID_995094431" MODIFIED="1454258794873" TEXT="evaluation and predicting the features of people">
+<font NAME="PingFang TC" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1454258746156" ID="ID_1687497899" MODIFIED="1454258749177" POSITION="right" TEXT="methods">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="PingFang TC" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1454258750766" ID="ID_219976031" MODIFIED="1454258750768" POSITION="right" TEXT="">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="PingFang TC" SIZE="18"/>
+</node>
+</node>
+</map>
